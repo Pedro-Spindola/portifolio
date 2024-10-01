b37a6e25a0cbc010ca8ab4a050fa5e432aad6d8d
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2024-10-01 00:00:14 -0300

    Atualizando

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -3,16 +3,18 @@ import Container from "./Container"
 import Card from "./Card"
 import Foto from "../assets/perfil.jpg"
 import NextGen from "../assets/ProjetoNextGen.jpg"
+import MiniGame from "../assets/ProjetoMiniJogos.jpg"
+import CocaCola from "../assets/ImagemProjetoCocaCola.jpg"
 import React, { useState } from 'react';
 
 const Projetos = () => {
   const [projetoAtivo, setProjetoAtivo] = useState(0);
 
   const projetos = [
-    { numero: "01", nome: "NEXTGEN DESIGN", foto: NextGen, comentario: "NextGen Design é uma landing page dedicada à mentoria em design, criada para apresentar informações essenciais sobre o serviço. Utilizando React e CSS-in-JS, desenvolvi uma interface clean e responsiva, focada na experiência do usuário. A página inclui detalhes sobre a mentoria, depoimentos de usuários e um formulário de contato, facilitando a conexão entre aspirantes a designers e mentores. Este projeto reflete minhas habilidades em front-end e meu interesse em apoiar o crescimento da comunidade de design." },
-    { numero: "02", nome: "PROJETO 02", foto: Foto, comentario: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id odio auctor, semper sem auctor, ullamcorper urna. Etiam eleifend fermentum quam, quis sodales tortor tincidunt id. Nunc vestibulum ullamcorper feugiat. Donec orci arcu, placerat quis leo sit amet, aliquam eleifend massa. Maecenas ornare felis quis neque faucibus, eu semper nisi ornare. Integer rhoncus nec augue ac cursus. Nullam blandit non purus sit amet vulputate. Donec euismod metus sed hendrerit sagittis. Fusce dictum ut erat quis iaculis. Etiam venenatis quam at elit porttitor facilisis. Ut lobortis hendrerit scelerisque." },
-    { numero: "03", nome: "PROJETO 03", foto: Foto, comentario: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id odio auctor, semper sem auctor, ullamcorper urna. Etiam eleifend fermentum quam, quis sodales tortor tincidunt id. Nunc vestibulum ullamcorper feugiat. Donec orci arcu, placerat quis leo sit amet, aliquam eleifend massa. Maecenas ornare felis quis neque faucibus, eu semper nisi ornare. Integer rhoncus nec augue ac cursus. Nullam blandit non purus sit amet vulputate. Donec euismod metus sed hendrerit sagittis. Fusce dictum ut erat quis iaculis. Etiam venenatis quam at elit porttitor facilisis. Ut lobortis hendrerit scelerisque." },
-    { numero: "04", nome: "PROJETO 04", foto: Foto, comentario: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id odio auctor, semper sem auctor, ullamcorper urna. Etiam eleifend fermentum quam, quis sodales tortor tincidunt id. Nunc vestibulum ullamcorper feugiat. Donec orci arcu, placerat quis leo sit amet, aliquam eleifend massa. Maecenas ornare felis quis neque faucibus, eu semper nisi ornare. Integer rhoncus nec augue ac cursus. Nullam blandit non purus sit amet vulputate. Donec euismod metus sed hendrerit sagittis. Fusce dictum ut erat quis iaculis. Etiam venenatis quam at elit porttitor facilisis. Ut lobortis hendrerit scelerisque." }
+    { numero: "01", nome: "NEXTGEN DESIGN", foto: NextGen, comentario: "NextGen Design é uma landing page dedicada à mentoria em design, criada para apresentar informações essenciais sobre o serviço. Utilizando React e CSS-in-JS, desenvolvi uma interface clean e responsiva, focada na experiência do usuário. A página inclui detalhes sobre a mentoria, depoimentos de usuários e um formulário de contato, facilitando a conexão entre aspirantes a designers e mentores. Este projeto reflete minhas habilidades em front-end e meu interesse em apoiar o crescimento da comunidade de design.", linkGit: "https://github.com/Pedro-Spindola/nextgen_Design", linkPage: "https://pedro-spindola.github.io/nextgen_Design/"},
+    { numero: "02", nome: "MINI-JOGOS", foto: MiniGame, comentario: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id odio auctor, semper sem auctor, ullamcorper urna. Etiam eleifend fermentum quam, quis sodales tortor tincidunt id. Nunc vestibulum ullamcorper feugiat. Donec orci arcu, placerat quis leo sit amet, aliquam eleifend massa. Maecenas ornare felis quis neque faucibus, eu semper nisi ornare. Integer rhoncus nec augue ac cursus. Nullam blandit non purus sit amet vulputate. Donec euismod metus sed hendrerit sagittis. Fusce dictum ut erat quis iaculis. Etiam venenatis quam at elit porttitor facilisis. Ut lobortis hendrerit scelerisque.", linkGit: "", linkPage: ""},
+    { numero: "03", nome: "LANDING PAGE - COCA COLA", foto: CocaCola, comentario: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id odio auctor, semper sem auctor, ullamcorper urna. Etiam eleifend fermentum quam, quis sodales tortor tincidunt id. Nunc vestibulum ullamcorper feugiat. Donec orci arcu, placerat quis leo sit amet, aliquam eleifend massa. Maecenas ornare felis quis neque faucibus, eu semper nisi ornare. Integer rhoncus nec augue ac cursus. Nullam blandit non purus sit amet vulputate. Donec euismod metus sed hendrerit sagittis. Fusce dictum ut erat quis iaculis. Etiam venenatis quam at elit porttitor facilisis. Ut lobortis hendrerit scelerisque.", linkGit: "", linkPage: ""},
+    { numero: "04", nome: "PROJETO 04", foto: Foto, comentario: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id odio auctor, semper sem auctor, ullamcorper urna. Etiam eleifend fermentum quam, quis sodales tortor tincidunt id. Nunc vestibulum ullamcorper feugiat. Donec orci arcu, placerat quis leo sit amet, aliquam eleifend massa. Maecenas ornare felis quis neque faucibus, eu semper nisi ornare. Integer rhoncus nec augue ac cursus. Nullam blandit non purus sit amet vulputate. Donec euismod metus sed hendrerit sagittis. Fusce dictum ut erat quis iaculis. Etiam venenatis quam at elit porttitor facilisis. Ut lobortis hendrerit scelerisque.", linkGit: "", linkPage: "" }
   ];
 
   return (
@@ -22,7 +24,7 @@ const Projetos = () => {
         <div className={styles.lineBoxProjetos}>
           {projetos.map((projeto, index) => (
             <React.Fragment key={projeto.numero}>
-              <div className={styles.selectProjeto} onClick={() => setProjetoAtivo(index)}>
+              <div className={`${styles.selectProjeto} ${index === 0 ? styles.selectProjeto1 : index === 1 ? styles.selectProjeto2 : index === 2 ? styles.selectProjeto3 : ''}`} onClick={() => setProjetoAtivo(index)}>
                 <h1 className={styles.h1NumeroProjeto}>{projeto.numero}</h1>
                 <h1 className={styles.h1NomeProjeto}>{projeto.nome}</h1>
               </div>
@@ -33,8 +35,8 @@ const Projetos = () => {
                   numeroProjeto={projeto.numero} 
                   nome={projeto.nome} 
                   comentario={projeto.comentario} 
-                  linkGit="" 
-                  linkPage=""
+                  linkGit={projeto.linkGit} 
+                  linkPage={projeto.linkPage} 
                 />
               </div>
             </React.Fragment>
